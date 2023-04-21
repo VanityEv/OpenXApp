2491cb01500b17944b2233e299c1b057f6d7d085
Paweł Satora <106479438+VanityEv@users.noreply.github.com>
2023-04-21 15:31:14 +0200

    Update CategoryTotalValues.helpers.ts
    Deleted comment

--- src/components/CategoryTotalValues/CategoryTotalValues.helpers.ts
+++ src/components/CategoryTotalValues/CategoryTotalValues.helpers.ts
@@ -2,10 +2,6 @@ import { Product } from "../../APIData.types";
 
 export function getCategoryValue(data: Product[]) {
   const categoriesOverview = new Map<string, number>();
-  /*const productsMap = new Map<number, number>();
-  if (!productsMap.has(product.id)) {
-    productsMap.set(product.id, product.price);
-  }*/
   let currentPrice, newPrice;
   data.forEach((product: Product) => {
     if (!categoriesOverview.has(product.category)) {
